027c5ef6249a2b3b3e90f7b31cfc96e724d34761
AnandKR84 <anand.kr84@gmail.com>
2021-10-04 20:36:43 +0530

    Draft solution

---
+++ AlgoExpertTest/IslandPerimeterTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AlgoExpertTest
+{
+    public class IslandPerimeterTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
