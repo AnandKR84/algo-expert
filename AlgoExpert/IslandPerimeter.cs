4a6552c46359f6804b41a2e4f464d6c223acc070
AnandKR84 <anand.kr84@gmail.com>
2021-10-05 18:05:22 +0530

    Solved First problem

--- AlgoExpert/IslandPerimeter.cs
+++ AlgoExpert/IslandPerimeter.cs
@@ -2,7 +2,67 @@
 
 namespace AlgoExpert
 {
-    public class IslandPerimeter
+    public class Solution
     {
+        public static int IslandPerimeter(int[][] grid)
+        {
+            int perimeter = 0;
+            for(int i = 0; i < grid.Length; i++)
+            {
+                for(int j = 0; j < grid[i].Length; j++)
+                {
+                    if(grid[i][j] == 1)
+                    {
+                        if(i==0)
+                        {
+                            ++perimeter;
+                        }
+                        else
+                        {
+                            if (grid[i - 1][j] == 0)
+                            {
+                                ++perimeter;
+                            }                            
+                        }
+
+                        if(j==0)
+                        {
+                            ++perimeter;
+                        }
+                        else
+                        {
+                            if(grid[i][j-1]==0)
+                            {
+                                ++perimeter;
+                            }
+                        }
+
+                        if(i == grid.Length-1)
+                        {
+                            ++perimeter;
+                        }
+                        else
+                        {
+                            if(grid[i+1][j] == 0)
+                            {
+                                ++perimeter;
+                            }
+                        }
+                        if (j == grid[i].Length - 1)
+                        {
+                            ++perimeter;
+                        }
+                        else
+                        {
+                            if(grid[i][j+1] ==0)
+                            {
+                                ++perimeter;
+                            }
+                        }
+                    }
+                }
+            }
+            return perimeter;
+        }
     }
 }
--- AlgoExpertTest/IslandPerimeterTest.cs
+++ AlgoExpertTest/IslandPerimeterTest.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System.Collections;
+using FluentAssertions;
 
 namespace AlgoExpertTest
 {
@@ -9,10 +11,29 @@ namespace AlgoExpertTest
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCaseSource(typeof(IslandTestCases))]
+        public void TestBestCase(int[][] source, int returnVal)
         {
-            Assert.Pass();
+            // Act
+            int perimeter = AlgoExpert.Solution.IslandPerimeter(source);
+
+            perimeter.Should().Equals(returnVal);
+        }
+
+        private class IslandTestCases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { new int[][] { new int[] { 1 } }, 4 };
+                yield return new object[] { new int[][] { new int[] { 0 } }, 0 };
+                yield return new object[] { new int[][] {
+                    new int[] { 0, 1, 0, 0 },
+                    new int[] { 1, 1, 1, 0 },
+                    new int[] { 0, 1, 0, 0 },
+                    new int[] { 1, 1, 0, 0 }
+                }, 16
+                };
+            }
         }
     }
 }
