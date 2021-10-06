b79e8d19ebd0d794770a23f779f66089e030a28c
AnandKR84 <anand.kr84@gmail.com>
2021-10-06 10:08:13 +0530

    Adding climbingstairs solution

---
+++ AlgoExpert/ClimbingStairs.cs
@@ -0,0 +1,24 @@
+﻿// https://leetcode.com/problems/climbing-stairs/
+
+namespace AlgoExpert
+{
+    public class ClimbingStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            return countWays(n);
+        }
+
+        static int countWays(int s)
+        {
+            return fib(s + 1);
+        }
+
+        static int fib(int n)
+        {
+            if (n <= 1)
+                return n;
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
---
+++ AlgoExpertTest/ClimbingStairsTest.cs
@@ -0,0 +1,29 @@
+﻿using AlgoExpert;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections;
+
+namespace AlgoExpertTest
+{
+    class ClimbingStairsTest
+    {
+        [TestCaseSource(typeof(ClimbingStairsUseCases))]
+        public void TestBestCase(int noOfStairs, int noOfWays)
+        {
+            ClimbingStairs stairs = new ClimbingStairs();
+            var ways = stairs.ClimbStairs(noOfStairs);
+            ways.Should().Equals(noOfWays);
+        }
+
+        private class ClimbingStairsUseCases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { 2, 2 };
+                yield return new object[] { 3, 3 };
+                yield return new object[] { 4, 5 };
+                yield return new object[] { 5, 8 };
+            }
+        }
+    }    
+}
