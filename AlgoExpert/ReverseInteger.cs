782906f55cf8f7e1cb3e1714e8115333980b6bd2
Anand K R <anand.kr84@gmail.com>
2021-10-06 23:27:27 +0530

    Reverse string solution

---
+++ AlgoExpert/ReverseInteger.cs
@@ -0,0 +1,41 @@
+﻿//https://leetcode.com/problems/reverse-integer/
+
+using System;
+
+namespace AlgoExpert
+{
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            int reversedInteger = 0;
+            bool negative = false;
+            if (x < 0)
+            {
+                negative = true;
+                x = Math.Abs(x);
+            }
+
+            do
+            {
+                if (reversedInteger * 10 / 10 == reversedInteger)
+                {
+                    reversedInteger = reversedInteger * 10 + x % 10;
+                    x /= 10;
+                }
+                else
+                {
+                    reversedInteger = 0;
+                    break;
+                }
+            } while (x > 0);
+
+            if (negative)
+            {
+                reversedInteger = reversedInteger * -1;
+            }
+
+            return reversedInteger;
+        }
+    }
+}
---
+++ AlgoExpertTest/ReverseIntegerTest.cs
@@ -0,0 +1,29 @@
+﻿using AlgoExpert;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections;
+
+namespace AlgoExpertTest
+{
+    public class ReverseIntegerTest
+    {
+        [TestCaseSource(typeof(UseCases))]
+        public void TestCases(int x, int reversed)
+        {
+            ReverseInteger revInteger = new ReverseInteger();
+            int returned = revInteger.Reverse(x);
+            Assert.AreEqual(reversed, returned);
+        }
+
+        private class UseCases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { 123, 321 };
+                yield return new object[] { -123, -321 };
+                yield return new object[] { 120, 21 };
+                yield return new object[] { 1534236469, 0 };
+            }
+        }
+    }
+}
