a99167c9644ae06dd750f1078516fcf347059b86
Anand K R <anand.kr84@gmail.com>
2021-10-06 14:54:18 +0530

    Adding solution for Two sum problem

---
+++ AlgoExpert/TwoSum.cs
@@ -0,0 +1,29 @@
+﻿https://leetcode.com/problems/two-sum/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoExpert
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            IList<int> indices = new List<int>();
+            IDictionary<int, Tuple<int, int>> lookupTable = new Dictionary<int, Tuple<int, int>>();
+            for(int i=0; i<nums.Length; i++)
+            {
+                if(lookupTable.ContainsKey(nums[i]))
+                {                    
+                    indices.Add(lookupTable[nums[i]].Item2);
+                    indices.Add(i);
+                    break;
+                }
+                lookupTable[target - nums[i]] = new Tuple<int, int>(nums[i], i);
+            }
+
+            return indices.ToArray();
+        }
+    }
+}
---
+++ AlgoExpertTest/TwoSumTest.cs
@@ -0,0 +1,27 @@
+﻿using AlgoExpert;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections;
+
+namespace AlgoExpertTest
+{
+    public class TwoSumTest
+    {
+        [TestCaseSource(typeof(TwoSumTestUseCases))]
+        public void TestCases(int[] numbers, int target, int[] indices)
+        {
+            TwoSumSolution twoSum = new TwoSumSolution();
+            var output = twoSum.TwoSum(numbers, target);
+            _ = output.Should().BeEquivalentTo(indices);
+        }
+
+        private class TwoSumTestUseCases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 } };
+                yield return new object[] { new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 } };
+            }
+        }
+    }
+}
