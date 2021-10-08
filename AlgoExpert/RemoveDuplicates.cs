5a7a207af1cba61f1e07fa55cab2eaa5a8ed70b5
Anand K R <anand.kr84@gmail.com>
2021-10-08 15:33:06 +0530

    remove duplicates problem

---
+++ AlgoExpert/RemoveDuplicates.cs
@@ -0,0 +1,25 @@
+﻿//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+namespace AlgoExpert
+{
+    public class RemoveDuplicatesSolution
+    {
+        public int RemoveDuplicates(int[] n)
+        {
+            if (n.Length == 0)
+            {
+                return 0;
+            }
+            int finalNo = 0;
+            for (int i = 1; i < n.Length; i++)
+            {
+                if(n[i] != n[finalNo])
+                {
+                    ++finalNo;                    
+                    n[finalNo] = n[i];
+                }
+            }
+            return finalNo+1;
+        }
+    }
+}
---
+++ AlgoExpertTest/RemoveDuplicatesSolutionTest.cs
@@ -0,0 +1,29 @@
+﻿using AlgoExpert;
+using NUnit.Framework;
+using System.Collections;
+
+namespace AlgoExpertTest
+{
+    public class RemoveDuplicatesSolutionTest
+    {
+        [TestCaseSource(typeof(UseCases))]
+        public void TestCases(int[] n, int expected)
+        {
+            RemoveDuplicatesSolution solution = new RemoveDuplicatesSolution();
+            var output = solution.RemoveDuplicates(n);
+            Assert.AreEqual(output, expected);
+        }
+
+        private class UseCases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { new int[] { }, 0 };
+                yield return new object[] { new int[] { 0, 0, 1 }, 2 };
+                yield return new object[] { new int[] { 0, 0, 0 }, 1 };
+                yield return new object[] { new int[] { 0, 1, 1, 2 }, 3 };
+                yield return new object[] { new int[] { 0, 1, 2, 3 }, 4 };
+            }
+        }
+    }
+}
