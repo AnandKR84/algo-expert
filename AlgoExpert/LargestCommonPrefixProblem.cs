5745ee9f18625a095e91e7d0d92a12bf91409d4d
Anand K R <anand.kr84@gmail.com>
2021-10-08 12:34:00 +0530

    Largest common prefix problem

---
+++ AlgoExpert/LargestCommonPrefixProblem.cs
@@ -0,0 +1,41 @@
+﻿//https://leetcode.com/problems/longest-common-prefix/
+
+namespace AlgoExpert
+{
+    public class LargestCommonPrefixProblem
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            string firstString = strs[0];
+            if(string.IsNullOrEmpty(firstString))
+            {
+                return string.Empty;
+            }
+            int i = 0;
+            do
+            {
+                bool notEqual = false;
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    string currString = strs[j];
+                    if (i >= currString.Length || firstString[i] != currString[i])
+                    {
+                        notEqual = true;
+                        break;
+                    }
+                }
+                if(notEqual)
+                {                    
+                    break; 
+                }
+                ++i;
+            } while (i < firstString.Length);
+
+            if(i>0)
+            {
+                return firstString.Substring(0, i);
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ AlgoExpertTest/LargestCommonPrefixTest.cs
@@ -0,0 +1,32 @@
+﻿using AlgoExpert;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections;
+
+namespace AlgoExpertTest
+{
+    public class LargestCommonPrefixTest
+    {
+        [TestCaseSource(typeof(UseCases))]
+        public void TestCases(string[] strs, string expectedResult)
+        {
+            LargestCommonPrefixProblem solution = new LargestCommonPrefixProblem();
+            var output = solution.LongestCommonPrefix(strs);
+            Assert.AreEqual(output, expectedResult);
+        }
+
+        private class UseCases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { new string[] { "flowers", "fleet", "flow"}, "fl" };
+                yield return new object[] { new string[] { "flowers", "fleet", "flight" }, "fl" };
+                yield return new object[] { new string[] { "dog", "dogcar", "car" }, string.Empty };
+                yield return new object[] { new string[] { "Drool", "Drool", "Drool" }, "Drool" };
+                yield return new object[] { new string[] { "#instagram", "#facebook", "#twitter" }, "#" };
+                yield return new object[] { new string[] { "ab", "a"}, "a" };
+
+            }
+        }
+    }
+}
