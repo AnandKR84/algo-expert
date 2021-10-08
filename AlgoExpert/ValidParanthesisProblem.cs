2d3b92ebff1dd91bbc763ee676a21c14d65ef3a0
Anand K R <anand.kr84@gmail.com>
2021-10-08 15:08:14 +0530

    valid paranthesis problem

---
+++ AlgoExpert/ValidParanthesisProblem.cs
@@ -0,0 +1,46 @@
+﻿//https://leetcode.com/problems/valid-parentheses/
+
+using System.Collections.Generic;
+
+namespace AlgoExpert
+{
+    public class ValidParanthesisProblem
+    {
+        public bool IsValid(string s)
+        {
+            if(string.IsNullOrEmpty(s))
+            {
+                return true;
+            }
+
+            HashSet<char> left = new HashSet<char> { '(', '[','{' };
+            HashSet<char> right = new HashSet<char> { ')', ']', '}' };
+            IDictionary<char, char> pair = new Dictionary<char, char>
+            {
+                [')'] = '(',
+                ['}'] = '{',
+                [']'] = '['
+            };
+
+            Stack<char> paranthesisValidator = new Stack<char>();
+
+            for (int i=0; i<s.Length; i++)
+            {
+                if(left.Contains(s[i]))
+                {
+                    paranthesisValidator.Push(s[i]);
+                    continue;
+                }
+                if(right.Contains(s[i]))
+                {
+                    if(paranthesisValidator.Count == 0 || paranthesisValidator.Pop() != pair[s[i]])
+                    {
+                        return false;
+                    }
+                }
+            }            
+
+            return paranthesisValidator.Count == 0;
+        }
+    }
+}
---
+++ AlgoExpertTest/ValidParanthesisTest.cs
@@ -0,0 +1,33 @@
+﻿using AlgoExpert;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections;
+
+namespace AlgoExpertTest
+{
+    public class ValidParanthesisTest
+    {
+        [TestCaseSource(typeof(UseCases))]
+        public void TestCases(string s, bool expected)
+        {
+            ValidParanthesisProblem solution = new ValidParanthesisProblem();
+            bool output = solution.IsValid(s);
+            Assert.AreEqual(output, expected);
+        }
+
+        private class UseCases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { "()", true };
+                yield return new object[] { "({})", true };
+                yield return new object[] { "({)}", false };
+                yield return new object[] { "(}", false };
+                yield return new object[] { "", true };
+                yield return new object[] { "()[]{}", true };
+                yield return new object[] { "(", false };
+                yield return new object[] { ")(", false };
+            }
+        }
+    }
+}
